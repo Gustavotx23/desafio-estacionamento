5f2251f5d89731b3c51a076c1ad8e278b5c7a6e3
Gustavotx2 <67023546+Gustavotx23@users.noreply.github.com>
2025-06-05 18:20:44 -0300

    Update Estacionamento.cs
    **Informações do veículo:** além da placa, armazena mais informações sobre cada veículo, como o tipo de veículo (carro, moto, bicicleta). Permite relatório mais detalhado da listagem de veículos no estacionamento.
    
    **Limite de capacidade:** implementado verificação para impedir a adição de veículos quando o estacionamento estiver cheio. Comparando se o método veiculos.Count, que retorna o número de veículos da coleção, se a quantidade de veículos é maior ou igual a 10.

--- Models/Estacionamento.cs
+++ Models/Estacionamento.cs
@@ -1,10 +1,16 @@
+using System.Globalization;
+using System.Security.Authentication.ExtendedProtection;
+using System.Security.Cryptography;
+using System;
+using System.Reflection.Metadata;
+
 namespace Projetos.Models
 {
     public class Estacionamento
     {
         private decimal precoInicial = 0;
         private decimal precoPorHora = 0;
-        private List<string> veiculos = new List<string>();
+        private List<Veiculo> veiculos = new List<Veiculo>(); 
 
         public Estacionamento(decimal precoInicial, decimal precoPorHora)
         {
@@ -12,19 +18,56 @@ namespace Projetos.Models
             this.precoPorHora = precoPorHora;
         }
 
+
         public void AdicionarVeiculo()
         {
             // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
             // *IMPLEMENTE AQUI*
 
-            Console.Clear();
-            Console.WriteLine("Digite a placa do veículo para estacionar:");
+            Veiculo veiculo = new Veiculo(); 
 
+            if (veiculos.Count >= 10) // Verifica se o estacionamento está cheio, maior ou igual a 10
+            {
+                Console.WriteLine($"Capacidade do estacionamento {veiculos.Count} excedida. Para adicionar um veículo, remova um.");
+                Console.ReadKey();
+                return; // Sai do método sem adicionar o veículo.
+            }
+
+
+            Console.WriteLine("Digite a placa do veículo para estacionar:"); // Agora pede a placa somente se houver espaço no estacionamento.
             string placa = Console.ReadLine();
-            veiculos.Add(placa);
             Console.Clear();
-            Console.WriteLine($"O veículo {placa} foi adicionado ao estacionamento");
 
+
+            Console.WriteLine("Preciso de mais informações... Qual seu tipo de veículo? Ex: carro, moto, bicicleta.");
+            string tipoVeiculo = Console.ReadLine().ToLower(); // Lê o input do usuário e o armazena, flexivel quanto a entrada maiusc/minusc.
+            switch(tipoVeiculo)
+            {
+                case "carro":
+                    Console.WriteLine($"O veículo do tipo carro da placa {placa} foi adicioando ao estacionamento.");
+                    veiculo.TipoVeiculo = "carro"; 
+                    veiculo.Placa = placa;
+                    veiculos.Add(veiculo); 
+                    break;
+                
+                case "moto":
+                    Console.WriteLine($"O veículo do tipo moto da placa {placa} foi adicioando ao estacionamento.");
+                    veiculo.TipoVeiculo = "moto";
+                    veiculo.Placa = placa;
+                    veiculos.Add(veiculo);
+                    break;
+
+                case "bicicleta":
+                    Console.WriteLine($"O veículo do tipo bicicleta da placa {placa} foi adicioando ao estacionamento.");
+                    veiculo.TipoVeiculo = "bicicleta";
+                    veiculo.Placa = placa;
+                    veiculos.Add(veiculo);
+                    break;
+
+                default:
+                    Console.WriteLine("Opção inválida. Tipo de veículo desconhecido.");
+                    break;
+            }
         }
 
         public void RemoverVeiculo()
@@ -34,15 +77,18 @@ namespace Projetos.Models
             // Pedir para o usuário digitar a placa e armazenar na variável placa
 
             // *IMPLEMENTE AQUI*
-            string placa = "";
 
-            placa = Console.ReadLine();
-            Console.WriteLine($"A placa do veículo é: {placa}");
+            string placa = Console.ReadLine();
+
+            // Procurar o veículo na lista
+        
+            Veiculo veiculoParaRemover = veiculos.FirstOrDefault(x => x.Placa.ToUpper() == placa.ToUpper()); 
             
             // Verifica se o veículo existe
-            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+
+            if (veiculoParaRemover != null)
             {
-                Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+                Console.WriteLine($"Placa encontrada. Digite a quantidade de horas que o veículo de placa {placa} permaneceu estacionado:");
                 // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
                 // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
                 // *IMPLEMENTE AQUI*
@@ -56,20 +102,20 @@ namespace Projetos.Models
                 // TODO: Remover a placa digitada da lista de veículos
                 // *IMPLEMENTE AQUI*
 
-                veiculos.Remove(placa);
+                veiculos.Remove(veiculoParaRemover);
                 Console.Clear();
-                Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+                Console.WriteLine($"O veículo da placa {placa} foi removido e o preço total foi de: R$ {valorTotal}");
             }
             else
             {
-                Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+                Console.WriteLine("Veículo não encontrado. Confira a placa digitada.");
             }
         }
 
         public void ListarVeiculos()
         {
             // Verifica se há veículos no estacionamento
-            if (veiculos.Any())
+            if (veiculos.Count>0)
             {
                 Console.Clear();
                 Console.WriteLine("Os veículos estacionados são: ");
@@ -78,9 +124,9 @@ namespace Projetos.Models
 
                 // *IMPLEMENTE AQUI*
                 int contadorForEach = 1;
-                foreach (string placa in veiculos)
+                foreach (var veiculo in veiculos)
                 {
-                    Console.WriteLine($"{contadorForEach} - {placa}");
+                    Console.WriteLine($"{contadorForEach}° - Tipo: {veiculo.TipoVeiculo} \t Placa: {veiculo.Placa}");
                     contadorForEach++;
                 }
             }
